3c29b990d48f6e9134551ce4912b09e001693c64
SP4R0W <sp4r0wxxx@gmail.com>
2025-01-14 22:10:56 +0100

    Added player movement and synchronizing

--- src/Game/Player/player.gd
+++ src/Game/Player/player.gd
@@ -6,26 +6,23 @@ class_name Player extends Area2D
 
 var speed: float = 200.0
 
-var sync_pos: Vector2 = Vector2.ZERO
-var sync_rot: float = 0
-
-#func _ready() -> void:
-	#set_multiplayer_authority(multiplayer.get_unique_id())
+@export var sync_pos: Vector2 = Vector2.ZERO
+@export var sync_rot: float = 0
 
 func _physics_process(delta: float) -> void:
 	if get_multiplayer_authority() != multiplayer.get_unique_id():
-		#global_position = global_position.lerp(sync_pos, 0.5)
-		#rotation_degrees = lerpf(rotation_degrees, sync_rot, 0.5)
+		global_position = global_position.lerp(sync_pos, synchronizer.replication_interval)
+		rotation_degrees = lerpf(rotation_degrees, sync_rot, synchronizer.replication_interval)
 		return
 
 	look_at(get_global_mouse_position())
 
-	#sync_pos = global_position
-	#sync_rot = rotation_degrees
-#
-	#var movement_vector: Vector2 = Input.get_vector("left","right","up","down")
-	#if movement_vector:
-		#global_position = Vector2(
-			#global_position.x + movement_vector.x * speed,
-			#global_position.y + movement_vector.y * speed
-		#)
+	sync_pos = global_position
+	sync_rot = rotation_degrees
+
+	var movement_vector: Vector2 = Input.get_vector("left","right","up","down")
+	if movement_vector:
+		global_position = Vector2(
+			global_position.x + movement_vector.x * speed * delta,
+			global_position.y + movement_vector.y * speed * delta
+		)
